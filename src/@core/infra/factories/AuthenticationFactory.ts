df65c1de8c1733eaefeb5e93e0fc27fe4d9cb3cf
Lucas Soares <icloud.lucasc@gmail.com>
2024-12-06 00:38:19 -0300

    chore: clean factory

--- src/@core/infra/factories/AuthenticationFactory.ts
+++ src/@core/infra/factories/AuthenticationFactory.ts
@@ -3,14 +3,12 @@ import { AxiosPostHttpClient } from "../http/axiosHttpClient/AxiosPostHttpClient
 import { AuthenticationService } from "../service/AuthenticationService";
 import { ZodAuthenticationValidator } from "../validators/authenticationValidator/ZodAuthenticationValidator";
 
-class MakeAuthentication {
-	static make() {
-		const client = new AxiosPostHttpClient();
-		const service = new AuthenticationService(client);
-		const validator = new ZodAuthenticationValidator();
-		const useCase = new AuhenticateUseCase(service, validator);
-		return useCase;
-	}
+function MakeAuthentication() {
+	const client = new AxiosPostHttpClient();
+	const service = new AuthenticationService(client);
+	const validator = new ZodAuthenticationValidator();
+	const useCase = new AuhenticateUseCase(service, validator);
+	return useCase;
 }
 
 export { MakeAuthentication };
